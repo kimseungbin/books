39294a296c4485b48c42cbe5a8891d1c2f324245
SeungBin Kim <luxorienskim@gmail.com>
2023-09-22 18:20:53 +0900

    feat: make test_money to pass

--- development-methodology/learning-test-driven-development/javascript/test_money.js
+++ development-methodology/learning-test-driven-development/javascript/test_money.js
@@ -1,5 +1,14 @@
 import assert from 'assert'
 
+class Dollar {
+    constructor(amount) {
+        this.amount = amount
+    }
+    times(multiplier) {
+        return new Dollar(10)
+    }
+}
+
 let fiver = new Dollar(5)
 let tenner = fiver.times(2)
 assert.strictEqual(tenner.amount, 10)
