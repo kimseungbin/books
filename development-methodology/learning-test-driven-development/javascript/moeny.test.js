4bf3261cd34e0125243d66e8bae8800cfd7c1e11
SeungBin Kim <luxorienskim@gmail.com>
2023-10-15 13:47:08 +0900

    test: add Division test

--- development-methodology/learning-test-driven-development/javascript/moeny.test.js
+++ development-methodology/learning-test-driven-development/javascript/moeny.test.js
@@ -23,3 +23,8 @@ let tenEuros = new Money(10, "EUR")
 let twentyEuros = tenEuros.times(2)
 assert.strictEqual(twentyEuros.amount, 20)
 assert.strictEqual(twentyEuros.currency, "EUR")
+
+let originalMoney = new Money(4002, "KRW")
+let actualMoneyAfterDivision = originalMoney.divide(4)
+let expectedMoneyAfterDivision = new Money(1000.5, "KRW")
+assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision)
