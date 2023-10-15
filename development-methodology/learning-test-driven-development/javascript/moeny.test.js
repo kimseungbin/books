49bcf6e6691ff6c8af7d503f542f7ddf2fd4f5d8
SeungBin Kim <luxorienskim@gmail.com>
2023-10-15 13:20:58 +0900

    refactor: replace Dollar class with Money class
    To comply with Don't Repeat Yourself principle, removed redundant class Dollar

--- development-methodology/learning-test-driven-development/javascript/moeny.test.js
+++ development-methodology/learning-test-driven-development/javascript/moeny.test.js
@@ -1,22 +1,6 @@
 import assert from 'assert'
 import test from 'node:test'
 
-class Dollar {
-    constructor(amount) {
-        this.amount = amount
-    }
-
-    times(multiplier) {
-        return new Dollar(this.amount * multiplier)
-    }
-}
-
-let fiver = new Dollar(5)
-let tenner = fiver.times(2)
-test('tenner should return 10', t => {
-    assert.strictEqual(tenner.amount, 10)
-})
-
 class Money {
     constructor(amount, currency) {
         this.amount = amount
@@ -27,6 +11,13 @@ class Money {
         return new Money(this.amount * multiplier, this.currency)
     }
 }
+
+let fiver = new Money(5)
+let tenner = fiver.times(2)
+
+test('tenner should return 10', t => {
+    assert.strictEqual(tenner.amount, 10)
+})
 
 let tenEuros = new Money(10, "EUR")
 let twentyEuros = tenEuros.times(2)
